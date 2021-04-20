0ec0ed961c57ef8ee34f62446e24559f3fec8fa3
Ziming Wu <s3678134@student.rmit.edu.au>
2021-04-20 22:47:51 +1000

    Update Theme - Initial project framework (using MVC)

--- view/user/login-unlay.php
+++ view/user/login-unlay.php
@@ -12,7 +12,7 @@
 <div id="app">
     <section class="section">
         <div class="d-flex flex-wrap align-items-stretch">
-            <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
+            <div class="col-lg-5 col-12 order-lg-1 min-vh-100 order-2 bg-white">
                 <div class="p-4 m-3">
                     <img src="assets/img/stisla-fill.svg" alt="logo" width="80" class="shadow-light rounded-circle mb-5 mt-2">
                     <h4 class="text-dark font-weight-normal">Welcome to <span class="font-weight-bold">STAFF SYSTEM</span></h4>
@@ -61,7 +61,7 @@
 
                 </div>
             </div>
-            <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="assets/img/unsplash/login-bg.jpg">
+            <div class="col-lg-7 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="assets/img/unsplash/login-bg.jpg">
                 <div class="absolute-bottom-left index-2">
                     <div class="text-light p-5 pb-2">
                         <div class="mb-5 pb-3">
